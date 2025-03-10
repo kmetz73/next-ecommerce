de6bec6501ece8a2abbb4f6fdbe7044fadcc4f1a
Kurt metzelfeld <kmetz73@gmail.com>
2025-03-10 13:03:46 -0500

    First commit Starter project add dynamic metadata

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Home',
+};
+
+const HomePage = () => {
+  return <div>Prostore</div>;
+};
+export default HomePage;
---
+++ lib/constants/index.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || '3d Plus';
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'A NEXT ecomerce  store starter';
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
