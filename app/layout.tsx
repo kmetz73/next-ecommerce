e71113d0745ec49ed2091c277b70f655378ce192
Kurt metzelfeld <kmetz73@gmail.com>
2025-03-10 17:06:37 -0500

    implemented Header, Footer, Dar and light toggle, Loading and  Not Found pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/assets/styles/globals.css';
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from '@/lib/constants';
+import { ThemeProvider } from 'next-themes';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,8 +21,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
+
+//  Example usage: for  a delayed loading page add this code to your page component
+// const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// const HomePage = async () => {
+//   await delay(2000); // Simulate some delay for the page to load
+//   return <div>Prostore</div>;
+// };
+// export default HomePage;
