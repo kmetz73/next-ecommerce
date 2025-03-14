24cbe314903223324dec773c96da070c4771da99
Kurt metzelfeld <kmetz73@gmail.com>
2025-03-14 16:46:29 -0500

    moved product Image into Link to  go to product details page

--- components/shared/product/Product-card.tsx
+++ components/shared/product/Product-card.tsx
@@ -8,14 +8,15 @@ const ProductCard = ({ product }: { product: Product }) => {
   return (
     <Card className="w-full max-w-sm">
       <CardHeader className="p-0 items-center">
-        <Link href={`/products/${product.slug}`}></Link>
-        <Image
-          src={product.images[0]}
-          alt={product.name}
-          width={300}
-          height={300}
-          priority={true}
-        />
+        <Link href={`/products/${product.slug}`}>
+          <Image
+            src={product.images[0]}
+            alt={product.name}
+            width={300}
+            height={300}
+            priority={true}
+          />
+        </Link>
       </CardHeader>
       <CardContent className="p-4 gap-4">
         <div className="text-xs">{product.brand}</div>
