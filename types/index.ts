200a85d50bc8799d6e3b8a8c4be8e68971297321
Kurt metzelfeld <kmetz73@gmail.com>
2025-03-17 15:28:35 -0500

    implemented cart scheme and validators

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,15 @@
 import { z } from 'zod';
-import { insertingProductSchema } from '@/lib/validators';
+import {
+  insertingProductSchema,
+  insertCartSchema,
+  cartItemSchema,
+} from '@/lib/validators';
 
 export type Product = z.infer<typeof insertingProductSchema> & {
   id: string;
   rating: string;
   createdAt: Date;
 };
+
+export type Cart = z.infer<typeof insertCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
