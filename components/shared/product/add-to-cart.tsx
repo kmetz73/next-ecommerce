2cec58789d2d0d8d7c5822721ce4ccca41a8d956
kmetz73 <kmetz73@gmail.com>
2025-03-21 11:53:20 -0400

    implemented  useTransition   for a loading spinner

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -3,48 +3,55 @@
 import { Button } from '@/components/ui/button';
 import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
 import { Cart, CartItem } from '@/types';
-import { PlusIcon, Minus } from 'lucide-react';
+import { PlusIcon, Minus, Loader } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import { useTransition } from 'react';
 import toast from 'react-hot-toast';
 
 const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
 
+  const [isPending, startTransition] = useTransition();
+
   const handelAddToCart = async () => {
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      const res = await addItemToCart(item);
 
-    if (!res.success) {
-      toast.error(res.message);
-      return;
-    }
+      if (!res.success) {
+        toast.error(res.message);
+        return;
+      }
 
-    // Handel successful add to cart
-    toast.custom((t) => (
-      <div className="bg-white shadow-md rounded-lg p-4 border border-gray-200 flex items-center space-x-4">
-        <span className=" text-gray-700">{res.message}</span>
-        <Button
-          onClick={() => {
-            router.push('/cart'); // Navigate to cart page
-            toast.dismiss(t.id); // Dismiss toast after clicking
-          }}
-          className="ml-auto bg-blue-500 text-white px-3 py-1 rounded-md hover:bg-blue-600 transition"
-        >
-          Add to Cart
-        </Button>
-      </div>
-    ));
+      // Handel successful add to cart
+      toast.custom((t) => (
+        <div className="bg-white shadow-md rounded-lg p-4 border border-gray-200 flex items-center space-x-4">
+          <span className=" text-gray-700">{res.message}</span>
+          <Button
+            onClick={() => {
+              router.push('/cart'); // Navigate to cart page
+              toast.dismiss(t.id); // Dismiss toast after clicking
+            }}
+            className="ml-auto bg-blue-500 text-white px-3 py-1 rounded-md hover:bg-blue-600 transition"
+          >
+            Add to Cart
+          </Button>
+        </div>
+      ));
+    });
   };
 
   // Handle Remove from cart
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
 
-    toast(res.message, {
-      style: res.success
-        ? {} // Default style (react-hot-toast uses this automatically)
-        : { background: 'red', color: 'white' }, // Custom destructive style
+      toast(res.message, {
+        style: res.success
+          ? {} // Default style (react-hot-toast uses this automatically)
+          : { background: 'red', color: 'white' }, // Custom destructive style
+      });
+      return;
     });
-    return;
   };
 
   // Check if item in cart
@@ -54,16 +61,28 @@ const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   return existItem ? (
     <div>
       <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
-        <Minus className="h-4 w-4" />
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Minus className="h-4 w-4" />
+        )}
       </Button>
       <span className="px-2">{existItem.qty}</span>
       <Button type="button" variant="outline" onClick={handelAddToCart}>
-        <PlusIcon className="h-4 w-4" />
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <PlusIcon className="h-4 w-4" />
+        )}
       </Button>
     </div>
   ) : (
     <Button className="w-full" type="button" onClick={handelAddToCart}>
-      <PlusIcon />
+      {isPending ? (
+        <Loader className="w-4 h-4 animate-spin" />
+      ) : (
+        <PlusIcon className="h-4 w-4" />
+      )}
       Add To Cart
     </Button>
   );
